ee654289dc8fa8fae92ccec540c59b775d53fd51
ljaycaliwan <caliwanlj27@gmail.com>
2022-08-28 09:44:53 +0800

    Initial Commit

---
+++ src/main/java/com/example/fileuploadbackend/FileUploadBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.fileuploadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadBackendApplication.class, args);
+    }
+}
